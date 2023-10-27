2dbd05504991ad8dd5164ea701f793aea6b6a010
martin <mtillmann@gmail.com>
2023-10-27 22:57:37 +0200

    wip

--- .vitepress/theme/components/Copyright.vue
+++ .vitepress/theme/components/Copyright.vue
@@ -1,12 +1,24 @@
 <template>
     <div class="site-footer">
+
+        <span xmlns:cc="http://creativecommons.org/ns#" xmlns:dct="http://purl.org/dc/terms/">
+            <a property="dct:title" rel="cc:attributionURL" href="https://mtillmann.github.io">{{ title }}</a>
+            by
+            <a rel="cc:attributionURL dct:creator" property="cc:attributionName" href="https://github.com/Mtillmann">Martin
+                Tillmann</a>
+
+            is licensed under
+
+            <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/?ref=chooser-v1" target="_blank"
+                rel="license noopener noreferrer" style="display:inline-block;">CC BY-NC-SA 4.0
+                <img src="/icons/cc.svg?ref=chooser-v1"><img src="/icons/by.svg?ref=chooser-v1"><img
+                    src="/icons/nc.svg?ref=chooser-v1"><img src="/icons/sa.svg?ref=chooser-v1"></a>
+        </span>
+        <br>
         Powered by <a class="vitepress" target="_blank" href="//vitepress.vuejs.org/">VitePress</a>
         &mdash;
-        Theme based on <a class="vitepress" target="_blank" href="//github.com/airene/vitepress-blog-pure">Airene's vitepress-blog-pure</a>
-        &mdash;
-        <span xmlns:cc="http://creativecommons.org/ns#" xmlns:dct="http://purl.org/dc/terms/">
-            <a property="dct:title" rel="cc:attributionURL" href="https://mtillmann.github.io">{{title}}</a> by <a rel="cc:attributionURL dct:creator" property="cc:attributionName" href="https://www.github.com/Mtillmann">Martin Tillmann</a> is licensed under <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/?ref=chooser-v1" target="_blank" rel="license noopener noreferrer" style="display:inline-block;">Attribution-NonCommercial-ShareAlike 4.0 International<img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1"><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1"><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/nc.svg?ref=chooser-v1"><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/sa.svg?ref=chooser-v1"></a>
-        </span>
+        Theme based on <a class="vitepress" target="_blank" href="//github.com/airene/vitepress-blog-pure">Airene's
+            vitepress-blog-pure</a>
     </div>
 </template>
 <script lang="ts" setup>
@@ -15,19 +27,28 @@ import { useData } from 'vitepress'
 const { site, theme } = useData()
 const url = theme.value.website
 const title = site.value.title
+
 </script>
 
 <style>
 .site-footer {
     color: #888;
     text-align: center;
-    font-size: 0.75rem;
+    font-size: 0.5rem;
     width: 100%;
     padding: 15px 0;
     overflow: auto;
-}
-.vitepress {
-    color: var(--vp-c-text-1);
-    font-weight: 700;
+
+    & a {
+        color: var(--vp-c-text-1) !important;
+        font-weight: 700;
+
+        & img {
+            display: initial;
+            height:12px!important;
+            margin-left:3px;
+            vertical-align:text-bottom;
+        }
+    }
 }
 </style>
